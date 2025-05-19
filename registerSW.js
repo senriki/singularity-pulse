4c8e04786d45961fa2be1f52785f3f4dda82d3c3
Senriki <59462950+senriki@users.noreply.github.com>
2025-05-19 20:23:02 +0700

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/singularity-pulse/sw.js', { scope: '/singularity-pulse/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
